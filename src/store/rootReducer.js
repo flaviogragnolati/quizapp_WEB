5b9bcd4e8d52318fd3b77186c7ae0a2bce624aa3
MG <marcosg1996@hotmail.com.ar>
2021-01-29 13:18:17 -0300

    register

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,11 +1,13 @@
 import actionsSlice from 'slices/actionsSlice';
 import favoritesSlice from 'views/Catalogue/favoritesSlice';
 import notificationSlice from 'views/Notifier/notificationSlice';
+import UserRegisterSlice from 'views/Register/registerSlice';
 
 const rootReducer = {
   notifications: notificationSlice.reducer,
   favorites: favoritesSlice.reducer,
   actions: actionsSlice.reducer,
+  User: UserRegisterSlice.reducer 
 };
 
 export default rootReducer;
--- src/views/Register/registerSlice.js
+++ src/views/Register/registerSlice.js
@@ -1,13 +1,14 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import {status} from '../../utils/helpers'
 import  axios  from 'axios';
 
 
 const initialState_user = {
-    user: {
+    allData: {
       info: {},
-      orders: [],
-      usersList: [],
-      status: 'idle',
+      token:'',
+      loginStatus:status.idle,
+      status: status.idle,
       error: null,
 
     },
@@ -15,29 +16,48 @@ const initialState_user = {
   
   export const createUser = createAsyncThunk('user/register', async (payload) => {
     const user_response = await axios.post('http://localhost:3000/auth/register', payload);
+    // const user_response = await fetch('http://localhost:3000/auth/register',
+    //  {method:'POST', body:JSON.stringify(payload),headers:{
+    //   'Content-Type': 'application/json'
+    // }});
+
     // const { token } = user_response.data;
     // const resPayload = {
     //   userRegister_response: user_response.data,
     // //   formik,
     // //   token,
     // };
-    return user_response;
+    console.log('tu vieja',user_response)
+    return user_response.data;
   });
 
 
 
-const notificationSlice = createSlice({
+const UserRegisterSlice = createSlice({
   name: 'user',
   initialState: initialState_user,
   reducers: {
-  },
+  },  
+    extraReducers: {
+    [createUser.pending]: (state, action) => {
+      state.allData.loginStatus = status.loading;
+    },
+    [createUser.fulfilled]: (state, {payload}) => {
+      state.allData.loginStatus = status.success;
+      state.allData.info = payload.user;   
+      state.allData.token = payload.token;     
+  
+
+    },
+    [createUser.rejected]: (state,action) => {
+      state.allData.loginStatus = status.error;
+
+    },
+
+
+  }
 });
 
-export const {
-  enqueueSnackbar,
-  closeSnackbar,
-  clearSnackbar,
-  removeSnackbar,
-} = notificationSlice.actions;
 
-export default notificationSlice;
+
+export default UserRegisterSlice;
