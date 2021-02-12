b011bd3b6cce99f7f123f64193e971ec47d60c3d
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2021-02-12 10:29:49 -0300

    403

--- src/views/School/TeachersQuiz/TeachersQuiz.jsx
+++ src/views/School/TeachersQuiz/TeachersQuiz.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Container, makeStyles } from '@material-ui/core';
+import { Box, Container, Link, makeStyles } from '@material-ui/core';
 import List from 'components/List';
 
 import Button from 'components/Home_MUI/Button';
@@ -8,6 +8,7 @@ import { userSelector, SchoolTeachersSelector } from 'utils/selectors';
 import { getTeachersQuiz } from 'views/School/SchoolSlice';
 import { SchoolStatusSelector } from 'utils/selectors';
 import BackdropLoading from 'components/Loading/BackdropLoading';
+import { useHistory } from 'react-router-dom';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -36,14 +37,15 @@ const TeachersQuiz = () => {
   const propsNames=['firstName','lastName','email']
   const dispatch = useDispatch()
   const school = useSelector(userSelector)
-  console.log(statusSchool)
+  const history  = useHistory()
+  
   return (
     <Container maxWidth={false}>
       {statusSchool === "success" ? 
       <Box mt={3}>
         <h3>Listado de Profesores de este Quiz</h3>
         {teachers.length === 0 ? <h2>No hay Profesores en este Quiz,puedes cargar profesores en el listado de Quizes</h2> :<List customers={teachers} columnName={columnName} propsNames={propsNames} />}
-      </Box> : <BackdropLoading/>}
+      </Box> : statusSchool === undefined ? <><h2>403 forbidden</h2><Button onClick={() =>history.push('/')} >Regresar al Inicio</Button></> : <BackdropLoading/>}
 
     </Container>
   );
