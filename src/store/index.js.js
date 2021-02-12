0f82a8145cf74e28d4fed29ce3f8445cc2cfe6a1
flaviogragnolati <flavio.gragnolat@gmail.com>
2021-02-12 12:51:01 -0300

    notificaciones

--- src/store/index.js.js
+++ src/store/index.js.js
@@ -6,8 +6,8 @@ import notificationMiddleware from './notificationMiddleware';
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    // getDefaultMiddleware().concat(notificationMiddleware).concat(logger),
-    getDefaultMiddleware().concat(logger),
+    getDefaultMiddleware().concat(notificationMiddleware).concat(logger),
+  // getDefaultMiddleware().concat(logger),
 });
 
 export default store;
