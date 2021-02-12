a35de04c1980b619009ae3e0b38fd97419f51194
MG <marcosg1996@hotmail.com.ar>
2021-02-11 23:06:33 -0300

    filtros del catalogo listos

--- src/views/Catalogue/components/SchoolFilterDetail.jsx
+++ src/views/Catalogue/components/SchoolFilterDetail.jsx
@@ -5,7 +5,7 @@ import CountryFilter from './CountryFilter';
 import { useSelector } from 'react-redux';
 import { schoolsSelector } from 'utils/selectors';
 
-function SchoolFilterDetail({ setFilter, values }) {
+function SchoolFilterDetail({ setFilter, values , clear}) {
   const schools = useSelector(schoolsSelector);
   const schoolOptions = formatStateToOptions(schools);
   // const getFilterValues = (values) => {
@@ -22,32 +22,33 @@ function SchoolFilterDetail({ setFilter, values }) {
         options={schoolOptions || [{ id: '', label: 'NO DATA' }]}
         // action={getFilterValues}
         values={values}
+        clear={clear}
         setFilter={setFilter}
         name="school"
       />
       <br></br>
-      <CountryFilter label="Pais" placeholder="Country" />
+      <CountryFilter clear={clear} label="Pais" placeholder="Country" />
     </>
   );
 }
 
 export default SchoolFilterDetail;
 
-const fakeSchools = [
-  { label: 'Universidad de Buenos Aires' },
-  { label: 'Universidad de la Plata' },
-  { label: 'Universidad Tecnologica Nacional' },
-  { label: 'Instituto Tecnologico Bs As' },
-  { label: 'Colegio 1' },
-  { label: 'Colegio 2' },
-  { label: 'Colegio 3' },
-  { label: 'Colegio 4' },
-  { label: 'Colegio 5' },
-  { label: 'Colegio 6' },
-  { label: 'Colegio 7' },
-  { label: 'Colegio 9' },
-  { label: 'Escuela 1 ' },
-  { label: 'Escuela 2 ' },
-  { label: 'Escuela 3 ' },
-  { label: 'Escuela 4 ' },
-];
+// const fakeSchools = [
+//   { label: 'Universidad de Buenos Aires' },
+//   { label: 'Universidad de la Plata' },
+//   { label: 'Universidad Tecnologica Nacional' },
+//   { label: 'Instituto Tecnologico Bs As' },
+//   { label: 'Colegio 1' },
+//   { label: 'Colegio 2' },
+//   { label: 'Colegio 3' },
+//   { label: 'Colegio 4' },
+//   { label: 'Colegio 5' },
+//   { label: 'Colegio 6' },
+//   { label: 'Colegio 7' },
+//   { label: 'Colegio 9' },
+//   { label: 'Escuela 1 ' },
+//   { label: 'Escuela 2 ' },
+//   { label: 'Escuela 3 ' },
+//   { label: 'Escuela 4 ' },
+// ];
